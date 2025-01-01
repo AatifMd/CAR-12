776d9e93857f8f42197333169f88c46e0ce5bf6c
@aatifmd7218 <mohammad.aatif@oxcytech.com>
2025-01-01 13:04:47 +0530

    commit

---
+++ Car24-frontend/.vite/deps/@mui_icons-material_Menu.js
@@ -0,0 +1,10 @@
+"use client";
+import {
+  Menu_default
+} from "./chunk-AZNS3DS2.js";
+import "./chunk-C6WWHQR7.js";
+import "./chunk-G3N46PKO.js";
+import "./chunk-DDNM7ENY.js";
+export {
+  Menu_default as default
+};
---
+++ Car24-frontend/.vite/deps/@mui_icons-material_Person.js
@@ -0,0 +1,10 @@
+"use client";
+import {
+  Person_default
+} from "./chunk-CS4XPOXZ.js";
+import "./chunk-C6WWHQR7.js";
+import "./chunk-G3N46PKO.js";
+import "./chunk-DDNM7ENY.js";
+export {
+  Person_default as default
+};
